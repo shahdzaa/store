2368a17c033c83496cf8d5a1fbc980073ffe95c9
Shahed Zaa <shahdzaa1246@gmail.com>
2025-04-23 21:48:07 +0300

    first commit

---
+++ database/seeders/UserSeeder.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use App\Models\User;
+use Illuminate\Database\Seeder;
+
+class UserSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        User::create([
+            'name'=>'Admin',
+            'email'=>'jjjjjt@gmail.com',
+            'password'=>Hash::make('123456yyy')
+        ]);
+    }
+}
